18c3422beacd4fb0b7fdc9d30364475b0441e25c
Abhidragneel <abhijithgail001@gmail.com>
2018-04-06 20:01:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/mec_mh/Mess.kt
@@ -0,0 +1,12 @@
+package com.example.hp.mec_mh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Mess : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mess)
+    }
+}
